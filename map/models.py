f05b2c2684a85a9a63b6d81b6e6aa905bfa10bd9
Ezequiel Casella <ecasellao@gmail.com>
2020-03-07 20:00:22 -0300

    Agregado de campos al modelo Parking y aplicado el comando de migracion para modificar la DB

--- map/models.py
+++ map/models.py
@@ -8,8 +8,15 @@ class Parking(models.Model):
     longitude = models.FloatField(default=0)
 
     hourRate = models.DecimalField(max_digits=6, decimal_places=2, default=0)
-    
 
+    address = models.CharField(max_length=200, default='')
+    phoneNumber = models.IntegerField(default=0)
+
+    carSlots = models.IntegerField(default=0)
+    openingHours = models.CharField(max_length=200, default='')
+
+    notes = models.CharField(max_length=400, default='')
+    
     def __str__(self):
         return self.name
 
