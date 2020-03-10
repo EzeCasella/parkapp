718c85f1abccb2dad972e9f245e3e5a27d2408f3
Ezequiel Casella <ecasellao@gmail.com>
2020-03-10 00:14:31 -0300

    Implementacion de AJAX request, para informacion de parkings

--- map/urls.py
+++ map/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('map/', views.map, name='map'),
     path('map/<int:parking_id>/', views.detail, name='detail'),
+    path('ajax/getParkingInfo', views.get_parking_info, name='get_parking_info')
 ]
--- map/views.py
+++ map/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.core import serializers
 
 # Create your views here.
@@ -27,3 +27,17 @@ def detail(request, parking_id):
         'sel_parking': sel_parking 
     }
     return render(request, 'map/map.html', context)
+
+def get_parking_info(request):
+    # request debe ser AJAX y metodo GET.
+    if  request.is_ajax and request.method == "GET":
+        # get parking_id del lado del cliente
+        parking_id = request.GET.get("parking_id", None)
+        # get parking from DB
+        parking = Parking.objects.filter(id = parking_id).first()
+        # serializar en JSON el parking
+        ser_parking = serializers.serialize('json', [ parking, ])
+        # envio al cliente
+        return JsonResponse({"parking": ser_parking}, status = 200)
+    
+    return JsonResponse({}, status = 400)
