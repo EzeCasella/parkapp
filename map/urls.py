c78e1b359502063b85cbc8cecff39352f7a880ab
Ezequiel Casella <ecasellao@gmail.com>
2020-03-13 02:04:19 -0300

    Implementación de reserva queda funcional, se podría trabajar en popup para reserva

--- map/urls.py
+++ map/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('map/', views.map, name='map'),
     path('map/<int:parking_id>/', views.detail, name='detail'), # Quedó obsoleta porque se utiliza get_parking_info
     path('ajax/getParkingInfo', views.get_parking_info, name='get_parking_info'),
-    path('makeSchedule', views.make_schedule, name='make_schedule')
+    path('newSchedule', views.make_schedule, name='new_schedule'),
+    path('postSchedule/<int:parking_id>', views.make_schedule, name='post_schedule')
 ]
--- map/views.py
+++ map/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse, JsonResponse
 from django.core import serializers
+from django.template.loader import render_to_string
 
 # Create your views here.
-from .models import Parking
+from .models import Parking, Schedule
 from .forms import ScheduleForm
 
 def index(request):
@@ -44,9 +45,9 @@ def get_parking_info(request):
     
     return JsonResponse({}, status = 400)
 
-def make_schedule(request):
+def make_schedule(request, parking_id=None):
     # if this is a POST request we need to process the form data
-    if request.method == 'POST':
+    if request.is_ajax and request.method == 'POST':
         # create a form instance and populate it with data from the request:
         form = ScheduleForm(request.POST)
         # check whether it's valid:
@@ -54,10 +55,35 @@ def make_schedule(request):
             # process the data in form.cleaned_data as required
             # ...
             # redirect to a new URL:
+            # schedule = Schedule()
+            parking = Parking.objects.filter(id = parking_id).first()
+
+            sched = Schedule(
+                parking = parking,
+                checkin_date = form.cleaned_data['checkin_date'],
+                checkin_time = form.cleaned_data['checkin_time'],
+                checkout_date = form.cleaned_data['checkout_date'],
+                checkout_time = form.cleaned_data['checkout_time'],
+                phone_number = form.cleaned_data['phone_number'],
+            )
+            sched.save()
             return redirect('map')
 
     # if a GET (or any other method) we'll create a blank form
-    else:
-        form = ScheduleForm()
+    elif  request.is_ajax and request.method == "GET":
+        # get parking_id del lado del cliente
+        parking_id = request.GET.get("parking_id", None)
+        # get parking from DB
+        if Parking.objects.filter(id = parking_id).exists():
+
+            parking = Parking.objects.filter(id = parking_id).first()
+            parking_name = parking.name
+
+            form = ScheduleForm()
+            rendered_form = render_to_string('map/schedule.html', request=request, context={'form': form, 'parking_id':parking_id})
+
+            return JsonResponse({"parking_name": parking_name, 'form': rendered_form}, status = 200)
+
+    return JsonResponse({}, status = 400)
 
     return render(request, 'map/schedule.html', {'form': form})
