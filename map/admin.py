7ca54ae320a687b619862a20476ded0dd063fd68
Ezequiel Casella <ecasellao@gmail.com>
2020-03-14 01:21:48 -0300

    Creacion y gestion de campos para visibilidad en tablas de admin. Tambien cambio en la hora del servidor para corresponder con Montevideo

--- map/admin.py
+++ map/admin.py
@@ -4,5 +4,30 @@ from django.contrib import admin
 
 from .models import Parking, Schedule
 
-admin.site.register(Parking)
-admin.site.register(Schedule)
+class ScheduleInline(admin.TabularInline):
+    model = Schedule
+    extra = 0
+
+
+class ParkingAdmin (admin.ModelAdmin):
+    list_display = ('name', 'hourRate', 'address', 'phoneNumber', 'carSlots', 'openingHours', 'notes')
+    search_fields = ['name']
+
+    inlines = [ScheduleInline]
+
+@admin.register(Schedule)
+class ScheduleAdmin(admin.ModelAdmin):
+    readonly_fields = ('id', 'parking')
+    list_display = ('id', 'parking', 'checkin_date', 'checkin_time', 'checkout_date', 'checkout_time', 'phone_number' )
+    
+    list_filter = ['checkin_date', 'parking']
+
+    fieldsets = [
+        ('Schedule',               {'fields': ['id', 'parking']}),
+        ('Date Information', {'fields':['checkin_date', 'checkin_time', 'checkout_date', 'checkout_time']}),
+        ('Contact Information', {'fields': ['phone_number']})
+    ]
+    
+
+
+admin.site.register( Parking, ParkingAdmin)
--- map/models.py
+++ map/models.py
@@ -32,3 +32,6 @@ class Schedule(models.Model):
 
 
     phone_number = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.parking.name
